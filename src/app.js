385122f31937291862e8dd4df6b339b71532f5f2
mikydees <mikydees2@gmail.com>
2022-10-15 00:01:04 +0000

    initialcommit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+let suit = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+let randomsuit = Math.floor(Math.random() * suit.length);
+let suitVal = suit[randomsuit];
+document.getElementById("top-suit").innerHTML = suitVal;
+document.getElementById("bottom-suit").innerHTML = suitVal;
+let randomNumber = Math.floor(Math.random() * numbers.length);
+document.getElementById("number").innerHTML = numbers[randomNumber];
+
+if (suitVal === "&hearts;" || suitVal === "&diams;") {
+  document.getElementById("top-suit").style.color = "red";
+  document.getElementById("bottom-suit").style.color = "red";
+}
+console.log(document.getElementById("top-suit").innerHTML);
